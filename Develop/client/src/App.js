adc539a1f2487f897dd5d345a33441b0725392a9
Sela Privette <selaprivette@yahoo.com>
2022-08-05 19:01:04 -0400

    slight changes

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -24,6 +31,7 @@ function App() {
           />
         </Routes>
       </>
+      </ApolloProvider>
     </Router>
   );
 }
